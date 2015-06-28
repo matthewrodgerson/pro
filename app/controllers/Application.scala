2ef917e36beb520b39b4a679cb8eb760f244b819
matthewrodgerson <matthew.rodgerson@jhc.co.uk>
2015-06-28 23:05:15 +0100

    Paging
    Paging

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,16 +3,25 @@ package controllers
 import play.api._
 import play.api.mvc._
 import dal._
+import domain._
 
 class Application extends Controller {
 
-    def searchProjects(searchTerm: String) = Action {
-    val projectDal = new dal.ProjectDal {}
-    val projList = projectDal.getProjects(searchTerm)
-    Ok(views.html.main("Project list")(views.html.list(projList,searchTerm)))
+    def searchProjects(searchTerm: String, perPage:Int, pageNo:Int) = //Action {
+    {
+      val projectDal = new dal.ProjectDal {}
+      val projList = projectDal.getProjects(searchTerm)
+      val subList = projList.take(10)
+    //Ok(views.html.main("Project list")(views.html.list(projList,searchTerm)))
+    displayProjList(perPage,pageNo,searchTerm)
   }
   
-  
+    def displayProjList(perPage:Int,pageNo:Int,searchTerm:String) = Action {
+      val projectDal = new dal.ProjectDal {}
+      val projList = projectDal.getProjects(searchTerm)
+      val subList = (projList.drop((pageNo-1)*perPage)).take(perPage)
+    Ok(views.html.main("Project list")(views.html.list(subList,searchTerm,perPage,pageNo)))
+  }
   //def index = {Action {
     
     //Ok(views.html.index("Your new application is ready."))
